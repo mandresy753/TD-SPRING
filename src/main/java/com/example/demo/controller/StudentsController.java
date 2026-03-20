63aa3ec7fcb46b8cbebf7201ea0fe6d3220b9e10
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-20 22:48:24 +0300

    feat: add GET /students endpoint in StudentsController.java

--- src/main/java/com/example/demo/controller/StudentsController.java
+++ src/main/java/com/example/demo/controller/StudentsController.java
@@ -3,9 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.Students;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +12,35 @@ import java.util.List;
 public class StudentsController {
 
     private List<Students> listStudents = new ArrayList<>();
-
+    List<String> studentsNames = new ArrayList<>();
     @PostMapping("/students")
     public ResponseEntity<List<String>> addStudent(@RequestBody List<Students> students) {
 
         listStudents.addAll(students);
-    List<String> studentsNames = new ArrayList<>();
-    for (Students student : listStudents) {
-        studentsNames.add(student.getFirstName() + " " + student.getLastName());
-    }
+
+        for (Students student : listStudents) {
+            studentsNames.add(student.getFirstName() + " " + student.getLastName());
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(studentsNames);
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<String> getAllStudents(
+            @RequestHeader(value = "Accept", required = false) String accept) {
+        if (accept == null || "text/plain".equals(accept)) {
+            StringBuilder result = new StringBuilder();
+            for (Students s : listStudents) {
+                result.append(s.getFirstName())
+                        .append(" ")
+                        .append(s.getLastName())
+                        .append("\n");
+            }
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(result.toString());
+        }
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body("Format non supporté");
     }
 }
