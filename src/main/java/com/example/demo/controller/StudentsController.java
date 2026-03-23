a70888bf49b672e854002e85cb47b3e10eef4240
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-23 18:49:38 +0300

    feat: add GET /students endpoint

--- src/main/java/com/example/demo/controller/StudentsController.java
+++ src/main/java/com/example/demo/controller/StudentsController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Students;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +30,34 @@ public class StudentsController {
         }
     }
 
+    @GetMapping( "/students")
+    public ResponseEntity<?> getAllStudents(
+            @RequestHeader(value = "Accept", required = false) String accept) {
+        try {
+            if (accept == null || accept.trim().isEmpty()) {
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body("Accept header is missing");
+            }
 
+            if ("application/json".equals(accept)) {
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(listStudents);
+            }
+
+            if ("text/plain".equals(accept)) {
+                return ResponseEntity.status(HttpStatus.OK)
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .body(listStudents.toString());
+            }
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                    .body("Unsupported Accept: " + accept);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erreur GET : " + e.getMessage());
+        }
+    }
 }
