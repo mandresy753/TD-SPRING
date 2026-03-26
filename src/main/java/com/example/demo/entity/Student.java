84e5dbb29e054a2d00be16260811c7d4ebc4cb49
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-26 06:28:23 +0300

    chore: add class Student

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String reference;
+}
